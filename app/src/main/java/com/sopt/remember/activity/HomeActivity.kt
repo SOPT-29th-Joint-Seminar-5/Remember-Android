a31b0f7e2622bf2c48842a117e834106754008d9
KxxHyoRim <rlagyfla0709@gmail.com>
2021-11-13 16:19:48 +0900

    [Add] Initation UI Files

--- app/src/main/java/com/sopt/remember/activity/HomeActivity.kt
+++ app/src/main/java/com/sopt/remember/activity/HomeActivity.kt
@@ -1,9 +1,10 @@
-package com.sopt.remember
+package com.sopt.remember.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sopt.remember.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/sopt/remember/activity/PostViewActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.remember.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sopt.remember.R
+
+class PostViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_view)
+    }
+}

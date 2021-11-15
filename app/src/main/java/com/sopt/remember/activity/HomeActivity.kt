a600e324795bbd91ebc5edbc99d733d36d991d03
KxxHyoRim <rlagyfla0709@gmail.com>
2021-11-16 05:48:50 +0900

    [Add] NavigationBar, HorizontalScrollView

--- app/src/main/java/com/sopt/remember/activity/HomeActivity.kt
+++ app/src/main/java/com/sopt/remember/activity/HomeActivity.kt
@@ -3,10 +3,23 @@ package com.sopt.remember.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sopt.remember.R
+import com.sopt.remember.databinding.ActivityHomeBinding
+import com.sopt.remember.fragment.CommunityFragment
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val fragment1 = CommunityFragment()
+
+        supportFragmentManager.beginTransaction().add(R.id.container, fragment1).commit()
+
+
+
     }
 }

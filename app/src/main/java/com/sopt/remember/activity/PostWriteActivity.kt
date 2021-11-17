b84196427fc5b8ce48436f78b0846d3060245e34
SSU\yb9854 <yb9854@naver.com>
2021-11-17 18:44:18 +0900

    [Add] activity_post_view.xml, PostViewActivity.kt

--- app/src/main/java/com/sopt/remember/activity/PostWriteActivity.kt
+++ app/src/main/java/com/sopt/remember/activity/PostWriteActivity.kt
@@ -1,5 +1,6 @@
 package com.sopt.remember.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -15,14 +16,30 @@ class PostWriteActivity : AppCompatActivity() {
         binding = ActivityPostWriteBinding.inflate(layoutInflater)
 
         clickBtnCategory()
+        clickBtnPosting()
         setContentView(binding.root)
+    }
+
+    private fun clickBtnPosting() {
+        binding.btPosting.setOnClickListener {
+            val title = binding.etTitle.text.toString()
+            val content = binding.etContent.text.toString()
+            if (title.isNotEmpty() && content.isNotEmpty()) {
+                startPostViewActivity(title, content)
+            }
+        }
+    }
+
+    private fun startPostViewActivity(title: String, content: String) {
+        val intent = Intent(this@PostWriteActivity, PostViewActivity::class.java)
+        intent.putExtra("title", title)
+        intent.putExtra("content", content)
+        startActivity(intent)
     }
 
     private fun clickBtnCategory() {
         binding.clSelectCategory.setOnClickListener {
             val bottomSheet = CategoryDialog()
-//            val bottomSheetBehavior = BottomSheetBehavior.from(binding.clSelectCategory)
-//            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
             bottomSheet.show(supportFragmentManager, bottomSheet.tag)
         }
     }
--- app/src/main/java/com/sopt/remember/fragment/CategoryDialog.kt
+++ app/src/main/java/com/sopt/remember/fragment/CategoryDialog.kt
@@ -1,15 +1,24 @@
 package com.sopt.remember.fragment
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.sopt.remember.R
 import com.sopt.remember.databinding.FragmentDialogCategoryBinding
 
 class CategoryDialog: BottomSheetDialogFragment() {
     private var _binding: FragmentDialogCategoryBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return BottomSheetDialog(requireContext(), R.style.NewDialog)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +26,8 @@ class CategoryDialog: BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentDialogCategoryBinding.inflate(layoutInflater, container, false)
+
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         return binding.root
     }
 

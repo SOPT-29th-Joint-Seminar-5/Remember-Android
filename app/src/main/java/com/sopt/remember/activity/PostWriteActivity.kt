29dd823c7215b279cb7c6a8b3e8530605978e2e5
SSU\yb9854 <yb9854@naver.com>
2021-11-17 13:02:23 +0900

    [Add] bottomSheetDialog height error

--- app/src/main/java/com/sopt/remember/activity/PostWriteActivity.kt
+++ app/src/main/java/com/sopt/remember/activity/PostWriteActivity.kt
@@ -2,8 +2,10 @@ package com.sopt.remember.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.sopt.remember.R
 import com.sopt.remember.databinding.ActivityPostWriteBinding
+import com.sopt.remember.fragment.CategoryDialog
 
 class PostWriteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPostWriteBinding
@@ -12,6 +14,16 @@ class PostWriteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPostWriteBinding.inflate(layoutInflater)
 
+        clickBtnCategory()
         setContentView(binding.root)
+    }
+
+    private fun clickBtnCategory() {
+        binding.clSelectCategory.setOnClickListener {
+            val bottomSheet = CategoryDialog()
+//            val bottomSheetBehavior = BottomSheetBehavior.from(binding.clSelectCategory)
+//            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+        }
     }
 }
---
+++ app/src/main/java/com/sopt/remember/fragment/CategoryDialog.kt
@@ -0,0 +1,27 @@
+package com.sopt.remember.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.sopt.remember.databinding.FragmentDialogCategoryBinding
+
+class CategoryDialog: BottomSheetDialogFragment() {
+    private var _binding: FragmentDialogCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDialogCategoryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}

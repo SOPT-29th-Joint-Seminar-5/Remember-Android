57fdf2c8f091653e338be8d0dfad301dfcf80bfc
KxxHyoRim <rlagyfla0709@gmail.com>
2021-11-18 04:05:15 +0900

    [Fix] Match viewPager with bottomNavigation Label

--- app/src/main/java/com/sopt/remember/activity/HomeActivity.kt
+++ app/src/main/java/com/sopt/remember/activity/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.sopt.remember.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.sopt.remember.R
@@ -48,21 +49,33 @@ class HomeActivity : AppCompatActivity() {
 
         binding.vpHome.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
-                binding.bnv.menu.getItem(position).isChecked = true
+
+                /** ViewPager 와 Bottom Navi 동기화 코드
+                 * position = viewPager의 idx
+                 * viewPager2의 2번째 페이지는 bottomNavi의 3번째 칸과 대응 (CommunityFrag)
+                 * viewPager2의 3번째 페이지는 bottomNavi의 4번째 칸과 대응 (MyPageFrag)
+                 * if, else if 문 구조 수정하지 마세요!
+                 * */
+                var btnIdx = position
+                if (btnIdx== 2) btnIdx = 3
+                else if (btnIdx == 3) btnIdx = 4
+
+                binding.bnv.menu.getItem(btnIdx).isChecked = true
             }
         })
 
         // 커뮤니티 디폴트로 설정
-        binding.vpHome.setCurrentItem(2)
+        binding.vpHome.currentItem = 2
         binding.bnv.menu.getItem(3).isChecked = true
 
         binding.bnv.menu.getItem(2).isEnabled = false
         binding.bnv.menu.getItem(2).isCheckable = false
 
+        //bottomNavigation 부분을 클릭했을때 작동
         binding.bnv.setOnItemSelectedListener {
             when(it.itemId) {
-                R.id.menu_bussinesscard -> {
-                    binding.vpHome.currentItem = FIRST_FRAGMENT
+                 R.id.menu_bussinesscard -> {
+                     binding.vpHome.currentItem = FIRST_FRAGMENT
                     return@setOnItemSelectedListener true
                 }
                 R.id.menu_career -> {
